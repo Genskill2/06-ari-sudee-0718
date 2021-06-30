401451027d9549b3d9ec7c2c8d8d995ec5021780
sudee-0718 <64221384+sudee-0718@users.noreply.github.com>
2021-07-01 00:01:34 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,2 +1,64 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<cs50.h>
+#include<ctype.h>
 
+string ari(string s)
+{
+	int characters=0, words=0, sentences=0;
+	for(int i=0;i<strlen(s);i++)
+	{
+		if (isalnum(s[i]))
+		{
+			characters++;
+		}
+		else if (s[i] == ' ')
+		{
+			words++;
+		}
+		else if (s[i] == '.' || s[i] == '?' || s[i] == '!')
+		{
+			sentences++;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	float answer = (4.71 * characters/words)+(0.5*words/sentences)-21.43;
+	int real = ceil(answer);
+	
+	switch (real)
+	{
+	case 1:
+		return "Kindergarten";
+	case 2:
+		return "First/Second Grade";
+	case 3:
+		return "Third Grade";
+	case 4:
+		return "Fourth Grade";
+	case 5:
+		return "Fifth Grade";
+	case 6:
+		return "Sixth Grade";
+	case 7:
+		return "Seventh Grade";
+	case 8:
+		return "Eighth Grade";
+	case 9:
+		return "Ninth Grade";
+	case 10:
+		return "Tenth Grade";
+	case 11:
+		return "Eleventh Grade";
+	case 12:
+		return "Twelfth grade";
+	case 13:
+		return "College Student";
+	case 14:
+		return "Professor";
+	}
+}
   
