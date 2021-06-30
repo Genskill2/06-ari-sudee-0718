d93702875e10fec7c77cb6599b33bebe72f1ad36
sudee-0718 <64221384+sudee-0718@users.noreply.github.com>
2021-06-30 23:31:43 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <cs50.h>
@@ -22,44 +21,56 @@ string ari(string s)
 		if (s[i] == '.' || s[i] == '?' || s[i] == '!')
 			count3++;
 	}
-	t = (4.71 * (count1 / count2) )+ (0.5 * (count2 / count3)) - 21.43;
+	t = 4.71 * (count1 / count2) + 0.5 * (count2 / count3) - 21.43;
 	int y = t + 1;
 	switch (y)
 	{
 	case 1:
 		return "Kindergarten";
-			
-			
+     break;
 	case 2:
 		return "First/Second Grade";
+      break;
 	case 3:
 		return "Third Grade";
+      break;
 	case 4:
 		return "Fourth Grade";
+    break;
 	case 5:
 		return "Fifth Grade";
+      break;
 	case 6:
 		return "Sixth Grade";
+      break;
 	case 7:
 		return "Seventh Grade";
+      break;
 	case 8:
 		return "Eighth Grade";
+     break;
 	case 9:
 		return "Ninth Grade";
+     break;
 	case 10:
 		return "Tenth Grade";
+     break;
 	case 11:
 		return "Eleventh Grade";
+     break;
 	case 12:
 		return "Twelfth grade";
+     break;
 	case 13:
 		return "College student";
+     break;
 	case 14:
 		return "Professor";
+     break;
 	default:
 		break;
 	}
 	return " ";
 
 }
- 
+
