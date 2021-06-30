d60e5324908e0eac39528f167d2bce088ce16697
sudee-0718 <64221384+sudee-0718@users.noreply.github.com>
2021-06-30 22:32:39 +0530

    Modified ari.c

--- ari.c
+++ ari.c
@@ -1,111 +1,56 @@
 string ari(string s)
 {
- int i;
- float cha=0.000000;
- float words=0.000000;
- float sentence=0.000000;
- for(i=0;s[i]!='\0';i++)
- {
-  if(isalnum(s[i]))
-  {
-    ++cha;
-  }
- }
+	int i = 0;
+	float t = 0;
+	float count1 = 0;
+	float count2 = 0;
+	float count3 = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (isalnum(s[i]))
+			count1++;
+		if (s[i] == ' ')
+			count2++;
+		if (s[i] == '.' || s[i] == '?' || s[i] == !)
+			count3++;
+	}
+	t = 4.71 * (count1 / count2) + 0.5 * (count2 / count3) - 21.43;
+	int y = t + 1;
+	switch (y)
+	{
+	case 1:
+		return "Kindergarten";
+	case 2:
+		return "First/Second Grade";
+	case 3:
+		return "Third Grade";
+	case 4:
+		return "Fourth Grade";
+	case 5:
+		return "Fifth Grade";
+	case 6:
+		return "Sixth Grade";
+	case 7:
+		return "Seventh Grade";
+	case 8:
+		return "Eighth Grade";
+	case 9:
+		return "Ninth Grade";
+	case 10:
+		return "Tenth Grade";
+	case 11:
+		return "Eleventh Grade";
+	case 12:
+		return "Twelfth grade";
+	case 13:
+		return "College student";
+	case 14:
+		return "Professor";
+	default:
+		break;
+	}
+	return " ";
 
-  for(i=0;s[i]!='\0';i++)
- {
-  if(s[i]=='.' || s[i]=='?' || s[i]=='!')
-  {
-    ++sentence;
-  }
- }
-
-  for(i=0;s[i]!='\0';i++)
- {
-  if(s[i]==' ')
-  {
-    ++words;
-  }
- }
-
- 
- float result;
- result= (4.71*cha/words) + (0.5*words/sentence) - 21.43;
- int x;
- string w;
- x=ceil(result);
- if(x==1)
- {
-  w= "Kindergarten";
-  return w;
- }
- if(x==2)
- {
-  w="First/Second Grade";
-  return w;
- }
- if(x==3)
- {
-  w="Third Grade";
-  return w;
- }
- if(x==4)
- {
- w="Fourth Grade";
- return w;
-}
- if(x==5)
-  {
- w="Fifth Grade";
- return w;
-}
- if(x==6)
-  {
- w="Sixth Grade";
- return w;
-}
- if(x==7)
-  {
- w="Seventh Grade";
- return w;
-}
- if(x==8)
-  {
- w="Eigth Grade";
- return w;
-}
- if(x==9)
- {
- w="Ninth Grade";
- return w;
-}
- if(x==10)
-  {
- w="Tenth Grade";
- return w;
-}
- if(x==11)
-  {
- w="Eleventh Grade";
- return w;
-}
- if(x==12)
- {
- w="Twelfth Grade";
- return w;
-}
- if(x==13)
- {
- w="College student";
- return w;
-}
- if(x==14)
- {
- w="Professor";
- return w;
 }
  
  
-
-
-}
